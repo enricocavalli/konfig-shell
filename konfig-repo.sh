1d387ef2887f917406b19f51dba1456a79d08b28
Enrico Cavalli <enrico.cavalli@gmail.com>
2013-12-08 14:52:27 +0100

    re-indentato konfig-repo per maggiore leggibilità

--- konfig-repo.sh
+++ konfig-repo.sh
@@ -4,48 +4,58 @@ KONFIG_GROUP=${KONFIG_GROUP:-"konfig"}
 REPOS_PATH=${REPOS_PATH:-"/usr/local/konfigrepo"}
 
 if [ -d "$REPOS_PATH" ]; then
-for path in "$@" ; do
+	for path in "$@" ; do
 
-if [ -d "$path" ]; then
-	if [ "$path" == "." ]; then
-		path=$(pwd)
-	fi
-dir_to_be_managed=${path%/} # remove trailing slash from arg
-repo_name=${dir_to_be_managed##*/}
+		if [ -d "$path" ]; then
+
+			if [ "$path" == "." ]; then
+				path=$(pwd)
+			fi
+
+		dir_to_be_managed=${path%/} # remove trailing slash from arg
+		repo_name=${dir_to_be_managed##*/}
 
 
-git init --bare --shared=group "$REPOS_PATH/$repo_name"
+		git init --bare --shared=group "$REPOS_PATH/$repo_name"
 
-cd "$dir_to_be_managed"
-echo "service=\"/etc/konfig/$repo_name\"" >> Konfigfile
-git init
-git add .
-git commit -m 'Initial commit'
+		cd "$dir_to_be_managed"
+		echo "service=\"/etc/konfig/$repo_name\"" >> Konfigfile
 
-git remote add origin "$REPOS_PATH/$repo_name"
-git config branch.master.remote origin
-git config branch.master.merge refs/heads/master
-git push origin master
+		git init
+		git add .
+		git commit -m 'Initial commit'
 
-mkdir -p /etc/konfig/$repo_name
+		git remote add origin "$REPOS_PATH/$repo_name"
+		git config branch.master.remote origin
+		git config branch.master.merge refs/heads/master
+		
+		git push origin master
 
-cat << EOF > /etc/konfig/$repo_name/00_pull.sh
+		mkdir -p /etc/konfig/$repo_name
+
+		cat << EOF > /etc/konfig/$repo_name/00_pull.sh
 cd $dir_to_be_managed
 git pull --rebase
 EOF
-chmod +x /etc/konfig/$repo_name/00_pull.sh
 
-cat << EOF > /etc/konfig/$repo_name/02_restart.sh
+		chmod +x /etc/konfig/$repo_name/00_pull.sh
+
+		cat << EOF > /etc/konfig/$repo_name/02_restart.sh
 /etc/init.d/$repo_name restart
 EOF
-chmod +x /etc/konfig/$repo_name/02_restart.sh
 
-cat << EOF > /etc/konfig/$repo_name/01_check_syntax.sh
+		chmod +x /etc/konfig/$repo_name/02_restart.sh
+
+		cat << EOF > /etc/konfig/$repo_name/01_check_syntax.sh
 true
 EOF
-chmod +x /etc/konfig/$repo_name/01_check_syntax.sh
-fi
-done
+
+		chmod +x /etc/konfig/$repo_name/01_check_syntax.sh
+		
+		fi
+	done
 else
-echo "Please create $REPOS_PATH with konfig-init.sh"
+
+	echo "Please create $REPOS_PATH with konfig-init.sh"
+
 fi
